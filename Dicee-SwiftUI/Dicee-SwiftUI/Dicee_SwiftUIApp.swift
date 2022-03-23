a1b7fa24b3e7fded15e348bf93cab45e0676e03c
Alexander <64682381+Alexander2990@users.noreply.github.com>
2022-03-23 17:37:30 +0500

    Add files via upload

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Александр on 23.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
